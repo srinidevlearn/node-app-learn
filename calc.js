e0647c8ebae4b98843b6cebdaeee91ec54896bc2
srinidevlearn <65352160+srinidevlearn@users.noreply.github.com>
2022-09-10 13:06:14 +0530

    node libuv demo
    node libuv demo

--- calc.js
+++
@@ -1,5 +0,0 @@
-function addTwo(inputNumber){
-    return inputNumber+2
-}   
-
-module.exports = { addTwo}
--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-const {addTwo} = require('./calc');
+const https = require('https');
+const crypto = require('crypto');
+const fs = require('fs');
 
-console.log(addTwo(2)) //4
+let start = Date.now();
+
+function doRequest(){
+    https.request('https://www.google.com',(res)=>{
+    res.on('data',(res,err)=>{
+        // var textChunk = res.toString('utf8');
+        // console.log(textChunk)
+    });
+    res.on('end',()=>{
+        console.log('Network',Date.now()-start)
+    });
+    }).end();
+}
+
+function doHash(){
+    return crypto.pbkdf2('123','my-app',100000,512,'sha512',()=>{
+        console.log('Hashed : ',Date.now()-start)
+    })
+}
+
+doRequest();node
+fs.readFile('index.js','utf-8',(err,res)=>{
+    console.log('FS:',Date.now()-start);
+}) 
+
+doHash();
+doHash();
+doHash();
+doHash();
+doHash();
+doHash();
 
 
-
+// const http = require('http');
+// http.createServer((req,res)=>{
+//     res.write('<p>Hello World</p>')
+// }).listen(5050)
